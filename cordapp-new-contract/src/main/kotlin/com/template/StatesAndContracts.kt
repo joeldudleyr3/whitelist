4d9e906aa9c221bd205efd93f53a90df279bcaeb
joeldudleyr3 <joel.dudley@r3.com>
2019-01-16 15:04:58 +0000

    Example of zone whitelists.

---
+++ cordapp-new-contract/src/main/kotlin/com/template/StatesAndContracts.kt
@@ -0,0 +1,25 @@
+package com.template
+
+import net.corda.core.contracts.CommandData
+import net.corda.core.contracts.Contract
+import net.corda.core.contracts.ContractState
+import net.corda.core.identity.Party
+import net.corda.core.transactions.LedgerTransaction
+
+class MyContract : Contract {
+    companion object {
+        const val ID = "com.template.MyContract"
+    }
+
+    override fun verify(tx: LedgerTransaction) {
+        // Verification logic goes here.
+    }
+
+    interface Commands : CommandData {
+        class Action : Commands
+    }
+}
+
+class State(val participant: Party) : ContractState {
+    override val participants = listOf(participant)
+}
---
+++ cordapp/src/main/kotlin/com/template/Flows.kt
@@ -0,0 +1,48 @@
+package com.template
+
+import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.flows.FinalityFlow
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.InitiatingFlow
+import net.corda.core.flows.StartableByRPC
+import net.corda.core.node.services.queryBy
+import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.ProgressTracker
+
+// *********
+// * Flows *
+// *********
+@InitiatingFlow
+@StartableByRPC
+class CreateState : FlowLogic<Unit>() {
+    override val progressTracker = ProgressTracker()
+
+    @Suspendable
+    override fun call() {
+        val notary = serviceHub.networkMapCache.notaryIdentities[0]
+        val txBuilder = TransactionBuilder(notary)
+                .addOutputState(State(ourIdentity), MyContract.ID)
+                .addCommand(MyContract.Commands.Action(), ourIdentity.owningKey)
+        txBuilder.verify(serviceHub)
+        val signedTransaction = serviceHub.signInitialTransaction(txBuilder)
+        subFlow(FinalityFlow(signedTransaction))
+    }
+}
+
+@InitiatingFlow
+@StartableByRPC
+class UpdateState : FlowLogic<Unit>() {
+    override val progressTracker = ProgressTracker()
+
+    @Suspendable
+    override fun call() {
+        val oldState = serviceHub.vaultService.queryBy<State>().states[0]
+        val txBuilder = TransactionBuilder(oldState.state.notary)
+                .addInputState(oldState)
+                .addOutputState(State(ourIdentity), MyContract.ID)
+                .addCommand(MyContract.Commands.Action(), ourIdentity.owningKey)
+        txBuilder.verify(serviceHub)
+        val signedTransaction = serviceHub.signInitialTransaction(txBuilder)
+        subFlow(FinalityFlow(signedTransaction))
+    }
+}
